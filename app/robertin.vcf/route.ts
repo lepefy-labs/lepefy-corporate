e99b5f3b982b0d2d66a1af4df378662b7ce9bca8
robertinsmartinvestor-gif <robertin.smartinvestor@gmail.com>
2026-08-29 02:00:15 +0200

    chore: redirect legacy personal vCard to brand vCard

--- app/robertin.vcf/route.ts
+++ app/robertin.vcf/route.ts
@@ -1,3 +1,3 @@
 export async function GET(request: Request) {
-  return Response.redirect(new URL('/founder.vcf', request.url), 308);
+  return Response.redirect(new URL('/card.vcf', request.url), 308);
 }
