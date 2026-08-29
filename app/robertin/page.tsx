f7224a56570185674f0ccb501cd51bbcebb7a3f0
robertinsmartinvestor-gif <robertin.smartinvestor@gmail.com>
2026-08-29 02:00:08 +0200

    chore: redirect legacy personal route to brand card

--- app/robertin/page.tsx
+++ app/robertin/page.tsx
@@ -1,5 +1,5 @@
 import { permanentRedirect } from 'next/navigation';
 
-export default function RobertinPage() {
-  permanentRedirect('/founder');
+export default function LegacyContactRedirect() {
+  permanentRedirect('/fr/card');
 }
